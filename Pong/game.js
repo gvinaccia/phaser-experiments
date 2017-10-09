936aeedac6dccf33e6c6ed5ed2f2ef0dc378b462
Alessandro Vinaccia <vinacciaalessandro@gmail.com>
2017-10-09 20:42:22 +0200

    test per push

--- Pong/game.js
+++ Pong/game.js
@@ -6,6 +6,8 @@ var game = new Phaser.Game(W, H, Phaser.CANVAS, 'container');
 game.state.add('menu', new MenuState());
 game.state.add('play', new PlayngState());
 game.state.add('gameover', new GameoverState());
+
+// avvia il gioco
 game.state.start('menu');
 
 
